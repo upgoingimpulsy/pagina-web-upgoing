ce1c2bc403a7d39e5a7fff266f6d1c64b383377a
impulsyco-spec <tu-email-de-github@gmail.com>
2026-06-02 16:02:50 -0500

    Fix lint errors causing Vercel build to fail

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Assistant from './components/Assistant';
 import ServicesPage from './pages/ServicesPage';
